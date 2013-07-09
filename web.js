1de9a8656f32528df916138a561716b3325bea59
Romain Caire <super.cairos@gmail.com>
2013-07-09 11:13:29 -0700

    Read index.html

--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+function readIndexHtml(){
+	var buffer = fs.readFile('index.html', 'utf-8');
+	return buffer.toString('utf-8');
+}
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(readIndexHtml());
 });
 
 var port = process.env.PORT || 5000;
